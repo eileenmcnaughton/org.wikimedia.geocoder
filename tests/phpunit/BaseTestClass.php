c54d52cedb6168b753eb6722781d0b57f5fa18b5
eileen <emcnaughton@wikimedia.org>
2019-06-12 10:03:38 -0400

    Phpunit 6 support

--- tests/phpunit/BaseTestClass.php
+++ tests/phpunit/BaseTestClass.php
@@ -18,7 +18,7 @@ use Civi\Test\TransactionalInterface;
  *
  * @group headless
  */
-class BaseTestClass extends \PHPUnit_Framework_TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
+class BaseTestClass extends \PHPUnit\Framework\TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
 
   public function setUpHeadless() {
     // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().

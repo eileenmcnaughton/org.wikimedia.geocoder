3dec4665bd46e2771747b2c7e88c4a8ccaf63186
Eileen McNaughton <emcnaughton@wikimedia.org>
2022-05-31 16:54:09 +1200

    Civix Update

--- geocoder.php
+++ geocoder.php
@@ -19,15 +19,6 @@ function geocoder_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function geocoder_civicrm_xmlMenu(&$files) {
-  _geocoder_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -82,18 +73,6 @@ function geocoder_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function geocoder_civicrm_managed(&$entities) {
-  _geocoder_civix_civicrm_managed($entities);
-}
-
-/**
  * Generate a list of geocoders to manage.
  *
  * In order to avoid having them installed when we do not wish we alter the
@@ -118,43 +97,9 @@ function geocoder_civicrm_geo_managed(&$entities) {
 }
 
 /**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function geocoder_civicrm_caseTypes(&$caseTypes) {
-  _geocoder_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function geocoder_civicrm_angularModules(&$angularModules) {
-  _geocoder_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function geocoder_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _geocoder_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Implements hook_civicrm_entityTypes.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
  *
  * @param array $entityTypes
  *   Registered entity types.
@@ -188,9 +133,8 @@ function geocoder_civicrm_alterLogTables(&$logTableSpec) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function geocoder_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().

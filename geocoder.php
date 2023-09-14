09ef077ef48327bc7e5c72d506524b856e5ad04a
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-09-14 17:46:18 +1200

    Civix update

--- geocoder.php
+++ geocoder.php
@@ -28,48 +28,12 @@ function geocoder_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function geocoder_civicrm_postInstall() {
-  _geocoder_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function geocoder_civicrm_uninstall() {
-  _geocoder_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function geocoder_civicrm_enable() {
   _geocoder_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function geocoder_civicrm_disable() {
-  _geocoder_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function geocoder_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _geocoder_civix_civicrm_upgrade($op, $queue);
 }
 
 /**

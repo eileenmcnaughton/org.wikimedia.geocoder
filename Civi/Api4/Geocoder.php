8b461894f0836450a558388e6f862732b3b58609
Matthew Wire <mjw@mjwconsult.co.uk>
2023-09-25 12:32:50 +0100

    Add searchkit form to view/edit geocoders

---
+++ Civi/Api4/Geocoder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Civi\Api4;
+
+/**
+ * Geocoder entity.
+ *
+ * Provided by the Geocoder extension.
+ *
+ * @search secondary
+ *
+ * @package Civi\Api4
+ */
+class Geocoder extends Generic\DAOEntity {
+
+}

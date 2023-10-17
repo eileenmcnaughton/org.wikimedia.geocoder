31d4cb22e3d4161a2b020b34fd362a96db944720
jmheitz <heitz.jm@gmail.com>
2023-10-17 12:46:47 +0200

    GEOCODER_45 : composer adjustments

--- api/v3/Geocoder/Geocode.php
+++ api/v3/Geocoder/Geocode.php
@@ -10,7 +10,7 @@ use Geocoder\Query\GeocodeQuery;
 function civicrm_api3_geocoder_geocode($params) {
 
   // currently not working
-  $httpClient = new \Http\Adapter\Guzzle6\Client();
+  $httpClient = new Http\Discovery\Psr18Client();
   $url = 'https://nominatim.openstreetmap.org';
   $provider = new Geocoder\Provider\Nominatim\Nominatim($httpClient, $url);
   $geocoder = new \Geocoder\StatefulGeocoder($provider, 'en');

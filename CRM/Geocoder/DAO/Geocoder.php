3b7ce105e0b3c62bed6f7c5e4ada6f165f4f2068
eileen <emcnaughton@wikimedia.org>
2025-06-19 13:31:16 +1200

    Whitespace fix

--- CRM/Geocoder/DAO/Geocoder.php
+++ CRM/Geocoder/DAO/Geocoder.php
@@ -8,20 +8,20 @@
  *
  * This stub provides compatibility. It is not intended to be modified in a
  * substantive way. Property annotations may be added, but are not required.
- * @property string $id 
- * @property string $name 
- * @property string $title 
- * @property string $class 
- * @property bool|string $is_active 
- * @property string $weight 
- * @property string $api_key 
- * @property string $url 
- * @property string $required_fields 
- * @property string $retained_response_fields 
- * @property string $datafill_response_fields 
- * @property int|string $threshold_standdown 
- * @property string $threshold_last_hit 
- * @property string $valid_countries 
+ * @property string $id
+ * @property string $name
+ * @property string $title
+ * @property string $class
+ * @property bool|string $is_active
+ * @property string $weight
+ * @property string $api_key
+ * @property string $url
+ * @property string $required_fields
+ * @property string $retained_response_fields
+ * @property string $datafill_response_fields
+ * @property int|string $threshold_standdown
+ * @property string $threshold_last_hit
+ * @property string $valid_countries
  */
 class CRM_Geocoder_DAO_Geocoder extends CRM_Geocoder_DAO_Base {
 

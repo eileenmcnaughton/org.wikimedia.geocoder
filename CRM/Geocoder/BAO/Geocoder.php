a8c014f98bfc037d250d6a5f2b3edaa6c0b35f1c
colemanw <coleman@civicrm.org>
2025-10-22 14:24:36 -0400

    Remove calls to deprecated CRM_Utils_Array::value
    Replaces deprecated function with equivalent null-coalescing operator. Behavior should be the same before/after.

--- CRM/Geocoder/BAO/Geocoder.php
+++ CRM/Geocoder/BAO/Geocoder.php
@@ -14,7 +14,7 @@ class CRM_Geocoder_BAO_Geocoder extends CRM_Geocoder_DAO_Geocoder implements \Ci
     $entityName = 'Geocoder';
     $hook = empty($params['id']) ? 'create' : 'edit';
 
-    CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
+    CRM_Utils_Hook::pre($hook, $entityName, $params['id'] ?? NULL, $params);
     $instance = new $className();
     $instance->copyValues($params);
     $instance->save();
